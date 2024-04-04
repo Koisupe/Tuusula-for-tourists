75a901934f29a849b883c71a08cfba0f0ecb253f
Koisupe <165833315+Koisupe@users.noreply.github.com>
2024-04-04 16:27:47 +0300

    Add files via upload

---
+++ html/index.js
@@ -0,0 +1,34 @@
+ ///et age = 3;
+ //console.log(`your age is ${old}`)
+ //$ on alt + 4
+ //let ikä = 13;
+ //plus ikä = ikä + 1;
+//miinus ikä = ikä - 1;
+//kerto ikä = ikä * 2;
+//jako ikä = ikä / 2;
+//potenssi ikä = ikä **2;
+//ylimääräiset jakaessa ikä = ikä % 2; 
+//ikä -= 1;
+//ikä += 1;
+//ikä /= 2;
+//ikä *= 2;
+//ikä **= 2;
+//ikä %= 2;
+// käyttää laskujärjestystä
+//window.alert(`moi`)
+//ikä = number(ikä); käyttäjien tiedon muuttamonen numeroiksi
+//sama toimii stringiksi ja booleaniksi muuttamisessa
+// const on muuttuja jota ei voi muuttaa
+//Math isolla M kertoo matemaattisia juttuja kuten piin
+//Math.PI
+
+let luokka;
+
+document.getElementById("lähetys").onclick = function(){
+    luokka = document.getElementById("joo").value
+    document.getElementById("p1").textContent = `Hello ${luokka}.`
+}
+
+
+
+
